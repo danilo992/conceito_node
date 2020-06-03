1faae5899d587cacfd2da33278eeded99f8d1382
danilo992 <tadeudanilo46@gmail.com>
2020-06-03 14:35:56 -0300

    Ultizando os metodos HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello word!!'});
-})
+app.get('/projetcs', (request, response) => {
+  return response.json([
+    'projeto 1',
+    'projeto 2',
+  ]);
+});
 
-app.listen(3333);
+app.post('/projetcs:id', (request, response) => {
+  return response.json([
+    'projeto 4',
+    'projeto 2',
+    'projeto 3',
+  ]);
+});
+
+app.put('/projetcs:id', (request, response) => {
+  return response.json([
+    'projeto 1',
+    'projeto 2',
+    'projeto 3',
+  ]);
+});
+
+app.delete('/projetcs:id', (request, response) => {
+  return response.json([
+    'projeto 1',
+    'projeto 2',
+    'projeto 3',
+  ]);
+});
+
+app.listen(3333, () => {
+  console.log('back-end started')
+});
